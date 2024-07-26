a01c3e7d9c4061864454fb59d65695b0d67f4e9f
umanga-rayamajhi <rayamajhiumanga@gmail.com>
2024-07-26 11:00:51 +0545

    array

--- templateArray.cpp
+++ templateArray.cpp
@@ -6,6 +6,14 @@ T findLargest(T arr[5]) {
         if (arr[i] > largest) {
             largest = arr[i];
         }
+    #include <iostream>
+template <class T>
+T findLargest(T arr[5]) {
+    T largest = arr[0];
+    for (int i = 1; i < 5; ++i) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
     }
     return largest;
 }
@@ -16,6 +24,19 @@ int main() {
 
     double doubleArr[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
     std::cout<< findLargest(doubleArr) << std::endl;
+
+    return 0;
+}
+}
+    return largest;
+}
+
+int main() {
+    int arr1[5] = {10, 20, 30, 40, 50};
+    std::cout<< findLargest(arr1) << std::endl;
+
+    double arr2[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    std::cout<< findLargest(arr2) << std::endl;
 
     return 0;
 }
